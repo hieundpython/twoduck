3ce6780ccd1b04bf7bbed5a8fcd0bb2873d29536
hieubk <hieundpython@gmail.com>
2024-01-23 00:36:24 +0700

    add parsing return statement

--- parser/parser.go
+++ parser/parser.go
@@ -46,12 +46,14 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
 }
 
-func (p *Parser) parseLetStatement() ast.Statement {
+func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt := &ast.LetStatement{Token: p.curToken}
 
 	if !p.expectPeek(token.IDENT) {
@@ -61,6 +63,16 @@ func (p *Parser) parseLetStatement() ast.Statement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
+	// TODO: We're skipping the expressions until we // encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	p.nextToken()
 	// TODO: We're skipping the expressions until we // encounter a semicolon
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
